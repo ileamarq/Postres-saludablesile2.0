43179b033ca7e319dd7910a106de93541d6c4ced
Ileana Marquez <168607179+ileamarq@users.noreply.github.com>
2024-06-28 12:03:32 +0000

    segundo commit

--- main.py
+++ main.py
@@ -2,44 +2,36 @@ import streamlit as st
 import requests
 import os
 
-st.header ("Aplicacion Postres Saludables") 
-consulta = st.text_input("Ingrese su consulta")
+st.header("Asistente para obtener Postres Saludables")
 
-if st.button('Enviar consulta'):
-      st.write(consulta)
-      
-base_url = "https://api.dify.ai/v1"
-path = "/completion-messages"
+text = st.text_area("Ingresa uno o varios ingredientes:")
 
 my_secret = os.environ['DIFY_APP_SECRET']
 
+
 headers = {
-'Authorization': f'Bearer {my_secret}', 
- 'Content-Type': 'application/json' 
+    'Authorization': f'Bearer {my_secret}',
+    'Content-Type': 'application/json'
 }
 
 data = {
-      "inputs": {"text": "consuta"},
-      "response_mode": "blocking",
-      "user": "ilepostres2"
-  }
+    "inputs": {
+        "text": text
+    },
+    "response_mode": "blocking",
+    "user": "ilepostres2"
+}
 
-response = requests.post(f'{base_url}{path}', json=data, headers=headers)
+if st.button('Consultar'):
+    base_url = "https://api.dify.ai/v1"
+    path = "/completion-messages"
+    response = requests.post(base_url + path, json=data, headers=headers)
+                       
+    if response.status_code == 200:
+          st.success('Consulta enviada con exito')
+    
+    result = response.json()
+    st.markdown('### Resultado de la solicitud:')
+    st.markdown(result['answer'])
 
-if response.status_code == 200:
-      st.success('Consulta enviada con exito')
-      st.json(response.json())
-      
-result = response.json()
-st.markdown('### Resultado de la solicitud:')
-st.markdown(result['answer'])
-
-
-      
-
-
-
-
-
-
-
+    print(response.content)
